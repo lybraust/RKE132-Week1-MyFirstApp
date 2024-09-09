0451e67d58b18533f7e66cc0fa37a84f85b1936e
Ly Braust <ly.braust@tktk.ee>
2024-09-09 12:11:42 +0300

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,11 @@
+﻿//This is a comment to my code
+
+//rakendus küsib kasutajal sisestada tema nime
+//rakendus ervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name:"); //Output
+string userName = Console.ReadLine(); //Input
+Console.WriteLine("Hello" +", "+ userName +"!"); //Output
+//string interpolation
+
+Console.WriteLine($"Hello, {userName}!"); //Output
